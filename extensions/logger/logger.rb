27b5d601f453ab0b8e1b5a09fbdbea660a4399ba
Rob Yurkowski <rob@yurkowski.net>
2011-09-25 14:10:36 -0600

    Updated logger.

--- extensions/logger/logger.rb
+++ extensions/logger/logger.rb
@@ -9,15 +9,36 @@ class ChatLogger < IRCClient
   
   def log_channel_message(target, user, message)
     date = Date.today
+    time = Time.now.strftime('%H:%M')
     folder = File.join(File.dirname(__FILE__), '..', '..', 'logs', date.year.to_s, date.month.to_s, date.day.to_s)
     
     # Make folder.
     FileUtils.mkdir_p(folder) unless File.directory?(folder) 
     
-    logfile = File.join(folder, target + '.txt')
+    logfile = File.join(folder, target + '.html')
   
-    FileUtils.touch(logfile) unless File.exists?(logfile)
-    File.open(logfile, 'a') {|f| f.puts "<#{user}> #{message}"}
+    unless File.exists?(logfile)
+      debug "Creating log file."
+      File.open(logfile, 'w') do |f|
+        first_write = <<-EOS
+<html>
+<head>
+  <title>#{date.year.to_s} - #{date.month.to_s} - #{date.day.to_s} -- #{target}</title>
+</head>
+<body>
+<table>
+  <tr>
+    <td class="timestamp">#{time}<td>
+    <td class="name">#{user}</td>
+    <td class="text">#{message}</td>
+  </tr>
+EOS
+      debug first_write
+      f.puts first_write
+      end
+    else
+      File.open(logfile, 'a') {|f| f.puts %(\t<tr>\n\t\t<td class="timestamp">#{time}</td>\n\t\t<td class="name">#{user}</td>\n\t\t<td class="text">#{message}</td>\n\t</tr>) }
+    end
   end
 end
 
