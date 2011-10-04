7db21607d0003614fb981ab7d6ce40b514b56962
Garrett Heinlen <heinleng@gmail.com>
2011-10-04 17:02:37 -0500

    added offline_messages feature

--- extensions/offline-message/offlinemessage.rb
+++ extensions/offline-message/offlinemessage.rb
@@ -1,11 +1,16 @@
 class OfflineMessages < IRCClient
 
   def initialize
-    add_hook(:join) {|m| send_message( m[:user] ) }
+    add_hook(:join) { |m| send_message( m[:user] ) }
   end
 
   def send_message(username)
     @messages = Message.where(username: username)
+    if @messages
+      @messages.each do |message|
+        say username, "#{message.username} asked me to tell you: #{message.body} - at #{message.created_at}"
+      end
+    end
   end
 
 end
