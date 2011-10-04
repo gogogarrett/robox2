26d9b97da4619f3f22a1ff4ce7ecdc52eeda0c35
Garrett Heinlen <heinleng@gmail.com>
2011-10-04 16:57:58 -0500

    added offline_messages method

---
+++ db/migrate/003_create_offline_messages.rb
@@ -0,0 +1,15 @@
+class OfflineMessages < ActiveRecord::Migration
+  def self.up
+    create_table :offline_messages do |t|
+      t.string :username
+      t.string :body
+      t.string :recipient
+
+      t.timestamps
+    end
+  end
+  
+  def self.down
+    drop_table :offline_messages
+  end
+end
