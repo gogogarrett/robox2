ace06f6855d262b6e8333b9d911952275b61f339
Garrett Heinlen <heinleng@gmail.com>
2011-10-04 20:23:19 -0500

    added confirmtion privatemessage after tell method

--- extensions/offline-message/offlinemessage.rb
+++ extensions/offline-message/offlinemessage.rb
@@ -6,22 +6,25 @@ class OfflineMessages < IRCClient
   end
 
   def route(target, user, command)
-    if /\Atell (\w+) ([\w\s]+)\Z/i.match command
-      save_message($1, $2)
+    if /\Atell (\S+)/i.match command
+      save_message($1, $', user)
     end
   end
 
   def send_message(username)
+    username = username.downcase
     @messages = OfflineMessage.where(username: username)
     if @messages
       @messages.each do |message|
         say username, "#{message.username} asked me to tell you: #{message.body} - at #{message.created_at}"
+        message.destroy
       end
     end
   end
 
-  def save_message(username, message)
+  def save_message(username, message, user)
     @message = OfflineMessage.create(username: username.downcase, body: message)
+    say user, "Your message to #{username} will be sent when they login."
   end
 
 end
